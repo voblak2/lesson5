9b27662b258d87dbc35035835efed2d5790062df
Vince Oblak <voblak2@gmail.com>
2021-03-04 16:08:00 -0600

    push changes in vs code

--- Program.cs
+++ Program.cs
@@ -2,11 +2,35 @@
 
 namespace lesson5
 {
+    public class Student
+    {
+        string firstName;
+        string lastName;
+        string courseFocus;
+
+        //Default constructor
+        public Student()
+        {
+            firstName = "";
+            lastName = "";
+            courseFocus = "";
+        }
+
+        // Parameterized constructor
+        public Student(string first, string last, string focus)
+        {
+            firstName = first;
+            lastName = last;
+            courseFocus = focus;
+
+            Console.WriteLine(firstName + " " + lastName + " is studying " + courseFocus);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Student student1 = new Student();
+            Student student2 = new Student("James", "Gosling", "C#");        }
     }
-}
+} //C# Lesson 5, Objects and Classes p4 under Properties
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # lesson5
-C# lesson 5 from Bethel School of Technology, on objects and classes.
+C# lesson 5 from Bethel School of Technology, on objects and classes. Program written using Visual Studio Community 2019 for Mac.
